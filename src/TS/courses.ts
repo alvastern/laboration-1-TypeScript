2d87f3674497280ca8fb57be751efe0035a06d5b
alvastern <alva.stern02@gmail.com>
2026-03-26 20:03:20 +0100

    Skapade funktionalitet för webbplatsen

--- src/TS/courses.ts
+++ src/TS/courses.ts
@@ -1,3 +1,4 @@
+// Interface för att definiera en kurs, datatyper definieras
 interface Courses {
     courseName: string;
     courseCode: string;
@@ -5,6 +6,7 @@ interface Courses {
     courseUrl: string;
 }
 
+// Funktion för att skapa en kurs - datatyper definieras
 function createCourse(courseName: string, courseCode: string, courseProg: string, courseUrl: string): Courses {
     return {
         courseName,
@@ -14,29 +16,58 @@ function createCourse(courseName: string, courseCode: string, courseProg: string
     };
 }
 
+// Hämtar element från DOM
 let name = document.getElementById("course-name") as HTMLInputElement;
 let code = document.getElementById("course-code") as HTMLInputElement;
 let prog = document.getElementById("course-prog") as HTMLInputElement;
 let url = document.getElementById("course-url") as HTMLInputElement;
 let form = document.getElementById("course-form") as HTMLFormElement;
 
+// Alla kurser sparas i denna array
 let courses: Courses[] = [];
 
+// Kursers som lagts till ska sparas i local storage, samt inte försvinna när sidan laddas om
+let savedCourses = localStorage.getItem("courses");
+if (savedCourses) {
+    courses = JSON.parse(savedCourses);
+};
+
+// När formuläret skickas skapas en kurs och läggs till i arrayen
 form.addEventListener("submit", (e) => {
     e.preventDefault();
 
+    // Skapa en kurs och lägg till i arrayen
     let course = createCourse(name.value, code.value, prog.value, url.value);
     courses.push(course);
     form.reset();
+
+    // Spara kurserna i local storage
+    localStorage.setItem("courses", JSON.stringify(courses));
+    displayCourses(courses);
 });
 
 let courseList = document.getElementById("course-list") as HTMLUListElement;
 
 function displayCourses(courses: Courses[]) {
     courseList.innerHTML = "";
-    courses.forEach((course) => {
+
+    courses.forEach((course, index) => {
         let list = document.createElement("li");
+
         list.textContent = `${course.courseName} (${course.courseCode}) - ${course.courseProg}`;
+        
+        let deleteButton = document.createElement("button");
+        deleteButton.textContent = "Radera kurs";
+
+        deleteButton.addEventListener("click", () => {
+            courses.splice(index, 1);
+            localStorage.setItem("courses", JSON.stringify(courses));
+            displayCourses(courses);
+        })
+
+        list.appendChild(deleteButton);
         courseList.appendChild(list);
     });
 };
+
+displayCourses(courses);
